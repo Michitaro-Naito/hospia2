708333fb2ac886508651f0b464e2b49fa8284e97
Michitaro Naito <m-naito@amlitek.com>
2014-03-21 04:54:58 +0900

    Fix: Settings range

--- app/Model/Settings.php
+++ app/Model/Settings.php
@@ -2,5 +2,27 @@
 class Settings extends AppModel{
 	public $name = 'Settings';
 	public $useTable = 'settings';
+	public $validate = array(
+		'insentive_hours'=>array(
+      'required' => array(
+          'rule' => array('notEmpty'),
+          'message' => '入力して下さい。'
+      ),
+      'range'=>array(
+      	'rule' => array('range', 0, 721),
+      	'message' => '1から720で入力して下さい。'
+			)
+		),
+		'insentive_max_count' => array(
+      'required' => array(
+          'rule' => array('notEmpty'),
+          'message' => '入力して下さい。'
+      ),
+      'range'=>array(
+      	'rule' => array('range', 0, 1000001),
+      	'message' => '1から1000000で入力して下さい。'
+			)
+		)
+	);
 }
 ?>
